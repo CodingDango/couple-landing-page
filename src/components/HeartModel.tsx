b3b20a9134f85304b4f3b0bbd5eb126ebe81d240
CodingDango <babyyodaman2.0@gmail.com>
2026-01-06 00:31:59 +0800

    cleared problems and changed icon

--- src/components/HeartModel.tsx
+++ src/components/HeartModel.tsx
@@ -1,11 +1,32 @@
-import { useGLTF } from '@react-three/drei'
+import * as THREE from 'three';
+import { useGLTF } from '@react-three/drei';
+import type { GLTF } from 'three-stdlib';
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Object_2: THREE.Mesh;
+    Object_3: THREE.Mesh;
+    Object_4: THREE.Mesh;
+    Object_5: THREE.Mesh;
+    Object_6: THREE.Mesh;
+    Object_7: THREE.Mesh;
+    Object_8: THREE.Mesh;
+    Object_9: THREE.Mesh;
+    Object_10: THREE.Mesh;
+    Object_11: THREE.Mesh;
+  }
+  materials: {
+    material_0: THREE.MeshStandardMaterial;
+  }
+}
 
 interface Props {
   scale: number;
 }
 
 function HeartModel(props : Props) {
-  const { nodes, materials } = useGLTF('/heart.glb')
+  const { nodes, materials } = useGLTF('/heart.glb') as unknown as GLTFResult;
+
   return (
     <group dispose={null} {...props}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
